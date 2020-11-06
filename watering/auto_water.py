4690030d580759e94ce0d826c36da90865f4d54e
wcheaqui <willcheaqui@yahoo.com>
2020-11-06 12:20:22 -0800

    bring current

--- watering/auto_water.py
+++ watering/auto_water.py
@@ -1,5 +1,13 @@
+
 import water
+import datetime
 
 
 if __name__ == "__main__":
+	newline = datetime.datetime.now().strftime('%b %d %Y at %H:%M:%S %p')
+	print(newline)
+	with open('last_watered.txt', 'a') as f:
+		f.write('\n')
+		f.write(newline)
 	water.auto_water()
+
--- watering/water.py
+++ watering/water.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import datetime
 import time
 
+
 init = False
 
 GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
@@ -9,42 +10,51 @@ GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
 def get_last_watered():
 	try:
 		f = open("last_watered.txt", "r")
-		return f.readline()
+		for line in f:
+			pass
+		print(line)
 	except:
-		return "Never watered"
+		print("Never watered")
 
 def get_status(pin = 8):
-	GPIO.setup(pin, GPIO.IN)
-	return GPIO.input(pin)
+	try:
+		GPIO.setup(pin, GPIO.IN)
+		print("moisture reading: ",GPIO.input(pin))
+		return GPIO.input(pin)
+	except:
+		print('Check Moisture Sensor')
 
 def init_output(pin):
 	GPIO.setup(pin, GPIO.OUT)
 	GPIO.output(pin, GPIO.LOW)
 	GPIO.output(pin, GPIO.HIGH)
 
-def auto_water(delay = 5, pump_pin = 7, water_sensor_pin = 8):
+def auto_water(delay = 5, pump_pin = 8, water_sensor_pin = 8):
 	consecutive_water_count = 0
 	init_output(pump_pin)
 	print("Here we go! Press CTRL + C to exit")
 	try:
-		while 1 and consecutive_water_count > 10:
+		while True and consecutive_water_count < 10:
 			time.sleep(delay)
 			wet = get_status(pin = water_sensor_pin) == 0
 			if not wet:
 				if consecutive_water_count < 5:
-					pump_on(pump_pin, 1)
+					pass #	pump_on(pump_pin, 1)
 				consecutive_water_count += 1
 			else:
 				consecutive_water_count = 0
 	except KeyboardInterrupt:
-		BPIO.cleanup()
+		GPIO.cleanup()
 
 
 def pump_on(pump_pin = 7, delay = 1):
 	init_output(pump_pin)
-	f = open("last_watered.txt", "w")
-	f.write("Last watered {}".format(datetime.datetime.now()))
+	f = open("last_watered.csv", "a")
+	f.write('\n')
+	f.write(datetime.datetime.now())
+	f.save()
 	f.close()
+
 	BPIO.output(pump_pin, GPIO.LOW)
 	time.sleep(delay)
 	GPIO.output(pump_pin, GPIO.HIGH)
