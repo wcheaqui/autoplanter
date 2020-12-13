b86a3b82b857f1cec2cf5c8fc4380af0eac988e9
wcheaqui <willcheaqui@yahoo.com>
2020-12-12 16:18:21 -0800

    bring up to date

---
+++ lighting/lights.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import datetime
+import time
+
+
+init = False
+
+GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
+
+
+def light_on(light_pin = 8, delay = 18):
+
+	now = datetime.datetime.now()
+
+	GPIO.setup(light_pin, GPIO.OUT)
+	GPIO.output(light_pin, GPIO.LOW)
+
+
+	try:
+
+		while True:
+			if ((now.hour >= 6) and (now.hour < 22)):
+
+				GPIO.output(light_pin, GPIO.HIGH)
+				GPIO.output(light_pin, GPIO.LOW)
+				print('Light is on: ', datetime.datetime.now())
+			else:
+				GPIO.output(light_pin, GPIO.HIGH)
+				print('Not time for light')
+			time.sleep(2)
+			now = datetime.datetime.now()
+
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+
+light_on()
--- watering/water.py
+++ watering/water.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import serial
 import pandas as pd
 import numpy as np
 
@@ -8,20 +9,16 @@ init = False
 
 GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
 
-def get_soil_moisture(channel, interval_for_readings=0.0001, number_of_intervals = 10):
-	import spidev
-	import time
 
-	spi = spidev.SpiDev()
-	spi.open(0,0)
+def get_soil_moisture(data = [], seri='/dev/ttyACM2'):
+	import serial
 
-	if channel > 7 or channel < 0:
-		return 'check ADC Channel'
-	r = spi.xfer([1, 8 + channel << 4, 0])
-	data = ((r[1] & 3) << 8 + r[2])
+	ser = serial.Serial(seri, 9600)
 
+	for i in range(100):
 
-	time.sleep(interval_for_readings)
+		print(int(ser.readline()))
+		data.append(int(ser.readline()))
 
 	return data
 
@@ -31,15 +28,19 @@ def sample_data(wet_or_dry, sample):
 	time_now = datetime.datetime.now()
 
 	if wet_or_dry=='wet':
-		df = pd.read_csv('wet.csv').iloc[:,0]
-		index = df.index
-		df = df.append(pd.Series(sample, index=[time_now]))
-#		print(df)
+#		df = pd.read_csv('wet.csv')
+		df = pd.DataFrame({'water_moisture_reading': sample})
+#		df = df.append(df_new)
+		df.to_csv('wet.csv', index=False)
+		print(df)
 		return df
 	elif wet_or_dry=='dry':
-		df = pd.read_csv('dry.csv').iloc[:,0]
-		index = df.index
-		df = df.append(pd.Series(sample, index=[time_now]))
+		df = pd.read_csv('dry.csv')
+		df_new = pd.DataFrame({'dry_moisture_reading': sample})
+		for i,r in df.iterrows():
+			print(r)
+		df = df.append(df_new)
+		df.to_csv('dry.csv', index=False)
 		return df
 
 def calibrate_sensor():
@@ -48,30 +49,24 @@ def calibrate_sensor():
 
 	time_now = datetime.datetime.now()
 
-	wet_readings = pd.read_csv('wet.csv').iloc[:,0]
-	dry_readings = pd.read_csv('dry.csv').iloc[:,0]
+	wet_readings = pd.read_csv('wet.csv')
+	dry_readings = pd.read_csv('dry.csv')
+
+	print(wet_readings)
 
 	Q1_wet = wet_readings.quantile(q=.25)
 	Q3_wet = wet_readings.quantile(q=.75)
 	IQR_wet = Q3_wet - Q1_wet
 
-	wet_readings = wet_readings[~((wet_readings < (Q1_wet-1.5*IQR_wet)) | (wet_readings > (Q3_wet+1.5*IQR_wet)))]
+	print(IQR_wet)
 
-	wet_readings_mean = statistics.mean(wet_readings)
-	print('wet average', wet_readings_mean)
-	wet_readings_std = statistics.stdev(wet_readings)
-	wet_readings_skew = wet_readings.skew(axis = 0)
+	Q1_dry = dry_readings.quantile(q=.25)
+	Q3_dry = dry_readings.quantile(q=.75)
+	IQR_dry = Q3_dry - Q1_dry
 
-	dry_readings_mean = statistics.mean(dry_readings)
-	print('dry average', dry_readings_mean)
-	dry_readings_std = statistics.stdev(dry_readings)
-	dry_readings_skew = dry_readings.skew(axis = 0)
+	print(IQR_dry)
 
-	print('wet std', wet_readings_std)
-	print('dry std', dry_readings_std)
 
-	print('wet skew', wet_readings_skew)
-	print('dry skew', dry_readings_skew)
 
 
 def get_last_watered():
@@ -83,61 +78,76 @@ def get_last_watered():
 	except:
 		print("Never watered")
 
-def get_status(pin = 8):
-	try:
-		GPIO.setup(pin, GPIO.IN)
-		print("moisture reading: ",GPIO.input(pin))
-		return GPIO.input(pin)
-	except:
-		print('Check Moisture Sensor')
-
 def init_output(pin):
 	GPIO.setup(pin, GPIO.OUT)
-	GPIO.output(pin, GPIO.LOW)
-	GPIO.output(pin, GPIO.HIGH)
 
-def auto_water(delay = 5, pump_pin = 8, water_sensor_pin = 8):
-	consecutive_water_count = 0
+
+def auto_water(pump_pin = 8, water_sensor_pin = 8):
+	import serial
+
+	ser = serial.Serial('/dev/ttyACM2', 9600)
+
+
+	water = 260
+	air = 574
+	percentage_unit = water - air
 	init_output(pump_pin)
-	print("Here we go! Press CTRL + C to exit")
+	place = 1
+
 	try:
-		while True and consecutive_water_count < 10:
-			time.sleep(delay)
-			wet = get_status(pin = water_sensor_pin) == 0
-			if not wet:
-				if consecutive_water_count < 5:
-					pass #	pump_on(pump_pin, 1)
-				consecutive_water_count += 1
-			else:
-				consecutive_water_count = 0
+		while True:
+			count = 0
+			try:
+				line = int(ser.readline())
+				count += 1
+			except:
+				continue
+			try:
+				line += int(ser.readline())
+				count += 1
+			except:
+				continue
+			try:
+				line += int(ser.readline())
+				count += 1
+			except:
+				continue
+
+			readings = (line/count * -1 + air)/(3.14)
+			if readings > 0 and readings < 110:
+				print('Moisture for plant 1: ', readings, '%')
+
+				now = datetime.datetime.now()
+
+				if ((now.hour == 11 or now.hour == 23) and (now.minute == 13)):
+					print('Checking Water for Sensor 1')
+
+					if readings < 50:
+						pump_on(pump_pin=7, delay=1)
+						print('Watered Space 1 at ', now.strftime('%Y-%m-%d %H:%M:%S'))
+
 	except KeyboardInterrupt:
 		GPIO.cleanup()
 
 
 def pump_on(pump_pin = 7, delay = 1):
 	init_output(pump_pin)
-	f = open("last_watered.csv", "a")
-	f.write('\n')
-	f.write(datetime.datetime.now())
-	f.save()
-	f.close()
+#	f = open("last_watered.csv", "a")
+#	f.write('\n')
+#	f.write(datetime.datetime.now().strftime("%b/%d/%Y %-I:%M:$S"))
+#	f.save()
+#	f.close()
 
-	BPIO.output(pump_pin, GPIO.LOW)
+	GPIO.output(pump_pin, GPIO.LOW)
 	time.sleep(delay)
 	GPIO.output(pump_pin, GPIO.HIGH)
-
-#moisture = 0
-
-#for i in range(1000):
-#	moisture -= get_soil_moisture(0)
-
-#reading = moisture/1000
-#reading = reading*10**-10
-#print(reading)
-
-#data = sample_data('dry', reading)
-#data.to_csv('dry.csv', index=False)
-#time.sleep(1)
+	time.sleep(delay)
 
 
-calibrate_sensor()
+#print(get_soil_moisture())
+# pump_on(pump_pin=10, delay=1)
+auto_water()
+
+
+
+#caliborate_sensor()
